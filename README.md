22c6dc5216c30c0e4ef94e635c452e4e94b69e19
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-06 22:48:07 +0800

    🎉Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![carefree-learn][socialify-image]
+
+`carefree-learn-deploy` are some production implementations for [`carefree-learn`](https://github.com/carefree0910/carefree-learn).
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+DESCRIPTION = "Production implementations for carefree-learn"
+with open("README.md", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="carefree-learn-deploy",
+    version=VERSION,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        "onnx",
+        "pillow",
+        "onnxruntime",
+        "scikit-image",
+        "scipy>=1.2.1",
+        "numpy>=1.19.2",
+    ],
+    author="carefree0910",
+    author_email="syameimaru.saki@gmail.com",
+    url="https://github.com/carefree0910/carefree-learn-deploy",
+    download_url=f"https://github.com/carefree0910/carefree-learn-deploy/archive/v{VERSION}.tar.gz",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords="python carefree-learn PyTorch",
+)
