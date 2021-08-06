35066eab8685c846d0e33d2b460f74ff0c6bcc6f
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-07 01:17:25 +0800

    ✅Enhanced `TestToolkit`

--- tests/test_toolkit.py
+++ tests/test_toolkit.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from cflearn_deploy.toolkit import cutout
+from cflearn_deploy.toolkit import is_gray
 
 
 class TestToolkit(unittest.TestCase):
@@ -10,6 +11,11 @@ class TestToolkit(unittest.TestCase):
         img = np.random.random([320, 320, 3])
         alpha = np.random.random([320, 320])
         cutout(img, alpha)
+
+    def test_is_gray(self) -> None:
+        self.assertTrue(is_gray(np.random.random([320, 320, 1])))
+        self.assertFalse(is_gray(np.random.random([320, 320, 3])))
+        self.assertFalse(is_gray(np.random.random([320, 320, 4])))
 
 
 if __name__ == "__main__":
