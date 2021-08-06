7e9bd9f88667184d2f68df7b45cc1ee39f02aeb3
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-08-07 01:14:02 +0800

    ✅Enhanced `TestSOD`

--- tests/test_sod.py
+++ tests/test_sod.py
@@ -10,9 +10,12 @@ current_folder = os.path.dirname(__file__)
 
 
 class TestSOD(unittest.TestCase):
-    def test_get_alpha(self) -> None:
+    def test_get_cutout(self) -> None:
         sod = SOD(os.path.join(current_folder, "models", "test.onnx"))
         sod._get_alpha(np.random.random([320, 320, 3]))
+        src_path = os.path.join(current_folder, "data", "pytorch.png")
+        tgt_path = os.path.join(current_folder, "data", "pytorch_cutout.png")
+        sod.generate_cutout(src_path, tgt_path)
 
 
 if __name__ == "__main__":
