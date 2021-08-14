784cdf60203b3c27593ef53c8c969569650dff7c
carefree0910 <syameimaru.saki@gmail.com>
2021-08-14 12:21:21 +0800

    ⚡️Introduced `CUDAExecutionProvider`

--- cflearn_deploy/sod/core.py
+++ cflearn_deploy/sod/core.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+from onnxruntime import SessionOptions
 from onnxruntime import InferenceSession
+from onnxruntime import GraphOptimizationLevel
 
 from .data import RescaleT
 from .data import ToNormalizedArray
@@ -16,15 +18,18 @@ class SOD:
         onnx_path: str,
         rescale_size: int = 320,
     ):
-        self.ort_session = InferenceSession(onnx_path)
+        options = SessionOptions()
+        options.intra_op_num_threads = 1
+        options.graph_optimization_level = GraphOptimizationLevel.ORT_ENABLE_ALL
+        self.sess = InferenceSession(onnx_path, options, ["CUDAExecutionProvider"])
         self.normalize = ToNormalizedArray()
         self.transform = Compose([RescaleT(rescale_size), self.normalize])
 
     def _get_alpha(self, src: np.ndarray, rescale: bool = True) -> np.ndarray:
         transform = self.transform if rescale else self.normalize
         transformed = transform({INPUT_KEY: src})[INPUT_KEY][None, ...]  # type: ignore
-        ort_inputs = {node.name: transformed for node in self.ort_session.get_inputs()}
-        logits = self.ort_session.run(None, ort_inputs)[0][0][0]
+        ort_inputs = {node.name: transformed for node in self.sess.get_inputs()}
+        logits = self.sess.run(None, ort_inputs)[0][0][0]
         logits = np.clip(logits, -50.0, 50.0)
         return 1.0 / (1.0 + np.exp(-logits))
 
