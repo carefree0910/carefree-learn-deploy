2284c71fcbc409dc86c652f9dc82342df11060c0
carefree0910 <syameimaru.saki@gmail.com>
2021-08-15 10:08:12 +0800

    ✨Extracted `ONNX`

---
+++ cflearn_deploy/onnx_api.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from typing import Dict
+from typing import Union
+from onnxruntime import SessionOptions
+from onnxruntime import InferenceSession
+from onnxruntime import GraphOptimizationLevel
+
+
+class ONNX:
+    def __init__(self, path: str):
+        options = SessionOptions()
+        options.intra_op_num_threads = 1
+        options.graph_optimization_level = GraphOptimizationLevel.ORT_ENABLE_ALL
+        self.sess = InferenceSession(
+            path,
+            options,
+            [
+                "CUDAExecutionProvider",
+                "OpenVINOExecutionProvider",
+            ],
+            [
+                {},
+                {"device_type": "CPU_FP32"},
+            ]
+        )
+
+    def run(self, inp: Union[np.ndarray, Dict[str, np.ndarray]]) -> np.ndarray:
+        if isinstance(inp, np.ndarray):
+            inp = {node.name: inp for node in self.sess.get_inputs()}
+        return self.sess.run(None, inp)
+
+
+__all__ = [
+    "ONNX",
+]
--- cflearn_deploy/sod/core.py
+++ cflearn_deploy/sod/core.py
@@ -1,14 +1,11 @@
 import numpy as np
 
-from onnxruntime import SessionOptions
-from onnxruntime import InferenceSession
-from onnxruntime import GraphOptimizationLevel
-
 from .data import RescaleT
 from .data import ToNormalizedArray
 from ..toolkit import cutout
 from ..toolkit import bytes_to_np
 from ..toolkit import Compose
+from ..onnx_api import ONNX
 from ..constants import INPUT_KEY
 
 
@@ -18,29 +15,14 @@ class SOD:
         onnx_path: str,
         rescale_size: int = 320,
     ):
-        options = SessionOptions()
-        options.intra_op_num_threads = 1
-        options.graph_optimization_level = GraphOptimizationLevel.ORT_ENABLE_ALL
-        self.sess = InferenceSession(
-            onnx_path,
-            options,
-            [
-                "CUDAExecutionProvider",
-                "OpenVINOExecutionProvider",
-            ],
-            [
-                {},
-                {"device_type": "CPU_FP32"},
-            ]
-        )
+        self.onnx = ONNX(onnx_path)
         self.normalize = ToNormalizedArray()
         self.transform = Compose([RescaleT(rescale_size), self.normalize])
 
     def _get_alpha(self, src: np.ndarray, rescale: bool = True) -> np.ndarray:
         transform = self.transform if rescale else self.normalize
         transformed = transform({INPUT_KEY: src})[INPUT_KEY][None, ...]  # type: ignore
-        ort_inputs = {node.name: transformed for node in self.sess.get_inputs()}
-        logits = self.sess.run(None, ort_inputs)[0][0][0]
+        logits = self.onnx.run(transformed)[0][0][0]
         logits = np.clip(logits, -50.0, 50.0)
         return 1.0 / (1.0 + np.exp(-logits))
 
