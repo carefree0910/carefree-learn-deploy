0989bc9031cff7dc35a4b3effd6d155d863035f5
carefree0910 <syameimaru.saki@gmail.com>
2021-08-29 18:21:17 +0800

    ✨Implemented `cbir`

--- apis/interface.py
+++ apis/interface.py
@@ -9,6 +9,7 @@ import logging.config
 
 from typing import Any
 from typing import Dict
+from typing import List
 from typing import Optional
 from typing import NamedTuple
 from fastapi import File
@@ -17,6 +18,11 @@ from fastapi import FastAPI
 from fastapi import Response
 from fastapi import HTTPException
 from pydantic import BaseModel
+from pymilvus_orm import connections
+from pymilvus_orm import DataType
+from pymilvus_orm import Collection
+from pymilvus_orm import FieldSchema
+from pymilvus_orm import CollectionSchema
 from cflearn_deploy.data import sqlite
 from cflearn_deploy.toolkit import np_to_bytes
 
@@ -80,6 +86,82 @@ def sod(img_bytes: bytes = File(...), data: SODModel = Depends()) -> Response:
         rgba = api_bundle.api.run(img_bytes, smooth=data.smooth, tight=data.tight)
         logging.debug(f"/cv/sod elapsed time : {time.time() - t:8.6f}s")
         return Response(content=np_to_bytes(rgba), media_type="image/png")
+    except Exception as err:
+        logging.exception(err)
+        e = sys.exc_info()[1]
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# cbir
+
+
+def get_cbir_collection() -> Collection:
+    connections.connect(host="localhost", port="19530")
+    fields = [
+        FieldSchema(name="id", dtype=DataType.INT64, is_primary=True),
+        FieldSchema(name="latent_code", dtype=DataType.FLOAT_VECTOR, dim=384),
+    ]
+    schema = CollectionSchema(fields=fields, description="dino vit collection")
+    return Collection(name="dino_vit", schema=schema)
+
+
+class CBIRModel(BaseModel):
+    top_k: int = 10
+    nprobe: int = 16
+    metric_type: str = "L2"
+    field_name: str = "latent_code"
+    model_name: Optional[str] = None
+    model_path: Optional[str] = None
+
+
+class LoadedEncoder(NamedTuple):
+    api: cflearn_deploy.ImageEncoder
+    path: str
+
+
+class CBIRResponse(BaseModel):
+    indices: List[int]
+    distances: List[float]
+
+
+@app.post("/cv/cbir", response_model=CBIRResponse)
+def cbir(img_bytes: bytes = File(...), data: CBIRModel = Depends()) -> CBIRResponse:
+    try:
+        logging.debug("/cv/cbir endpoint entered")
+        t1 = time.time()
+        key = "dino_vit"
+        api_bundle = model_zoo.get(key)
+        if data.model_path is not None:
+            model_path = data.model_path
+        else:
+            model_name = data.model_name or key
+            model_path = os.path.join(model_root, f"{model_name}.onnx")
+        if api_bundle is None or api_bundle.path != model_path:
+            api = cflearn_deploy.ImageEncoder(model_path)
+            api_bundle = model_zoo[key] = LoadedEncoder(api, model_path)
+        latent_code = api_bundle.api.run(img_bytes)
+        t2 = time.time()
+        collection = get_cbir_collection()
+        t3 = time.time()
+        res = collection.search(
+            [latent_code.tolist()],
+            data.field_name,
+            dict(metric_type=data.metric_type, params={"nprobe": data.nprobe}),
+            data.top_k,
+            output_fields=["id"],
+        )
+        hits = res[0]
+        t4 = time.time()
+        logging.debug(
+            f"/cv/cbir elapsed time : {t3 - t1:8.6f}s "
+            f"| onnx : {t2 - t1:8.6f} "
+            f"| milvus_init : {t3 - t2:8.6f} "
+            f"| milvus : {t4 - t3:8.6f} |"
+        )
+        return CBIRResponse(
+            indices=[hit.id for hit in hits],
+            distances=[hit.distance for hit in hits],
+        )
     except Exception as err:
         logging.exception(err)
         e = sys.exc_info()[1]
