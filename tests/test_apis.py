2d8101dfd87edb82218875d90e9300a4a2cd69cf
carefree0910 <syameimaru.saki@gmail.com>
2021-09-11 22:12:23 +0800

    🐛Fixed legacy typo

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -17,8 +17,8 @@ current_folder = os.path.dirname(__file__)
 class TestAPIs(unittest.TestCase):
     def test_adain(self) -> None:
         img = np.random.random([224, 224, 3])
-        model_path = os.path.join(current_folder, "models", "adain_test.onnx")
-        kwargs = _get_img_post_kwargs(img, img, model_path=model_path)
+        onnx_path = os.path.join(current_folder, "models", "adain_test.onnx")
+        kwargs = _get_img_post_kwargs(img, img, onnx_path=onnx_path)
         response = client.post("/cv/adain", **kwargs)
         self.assertEqual(response.status_code, 200)
         stylized = bytes_to_np(response.content, mode="RGB")
@@ -26,8 +26,8 @@ class TestAPIs(unittest.TestCase):
 
     def test_sod(self) -> None:
         img = np.random.random([320, 320, 3])
-        model_path = os.path.join(current_folder, "models", "sod_test.onnx")
-        kwargs = _get_img_post_kwargs(img, model_path=model_path)
+        onnx_path = os.path.join(current_folder, "models", "sod_test.onnx")
+        kwargs = _get_img_post_kwargs(img, onnx_path=onnx_path)
         response = client.post("/cv/sod", **kwargs)
         self.assertEqual(response.status_code, 200)
         rgba = bytes_to_np(response.content, mode="RGBA")
@@ -35,11 +35,11 @@ class TestAPIs(unittest.TestCase):
 
     def test_cbir(self) -> None:
         img = np.random.random([224, 224, 3])
-        model_path = os.path.join(current_folder, "models", "cbir_test.onnx")
+        onnx_path = os.path.join(current_folder, "models", "cbir_test.onnx")
         kwargs = _get_img_post_kwargs(
             img,
             task="cbir",
-            model_path=model_path,
+            onnx_path=onnx_path,
             skip_faiss=True,
         )
         response = client.post("/cv/cbir", **kwargs)
