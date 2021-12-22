f0a4ef54a8573f655904197949954ba8bf7f78d0
carefree0910 <syameimaru.saki@gmail.com>
2021-12-22 19:53:39 +0800

    ✨Implemented `cbir.material`

---
+++ apis/models/cbir.material/0/model.py
@@ -0,0 +1,40 @@
+import os
+
+import triton_python_backend_utils as pb_utils
+
+from typing import Any
+from typing import Dict
+from typing import List
+from cflearn_deploy.toolkit import IRMixin
+from cflearn_deploy.data.transforms import ImagenetPreprocess
+
+
+class TritonPythonModel(IRMixin):
+    appendix_list = [".material"]
+
+    def initialize(self, _: Dict[str, Any]) -> None:
+        self.preprocess = ImagenetPreprocess()
+        self.init_faiss("cbir", os.path.dirname(__file__))
+
+    def execute(
+        self,
+        requests: List[pb_utils.InferenceRequest],
+    ) -> List[pb_utils.InferenceResponse]:
+        responses = []
+        for request in requests:
+            array = pb_utils.get_input_tensor_by_name(request, "input").as_numpy()
+            top_k = pb_utils.get_input_tensor_by_name(request, "top_k").as_numpy()
+            n_probe = pb_utils.get_input_tensor_by_name(request, "num_probe").as_numpy()
+            top_k, n_probe = top_k.item(), n_probe.item()
+            array = self.preprocess(array, False)
+            m_request = pb_utils.InferenceRequest(
+                model_name=f"cbir.material_core",
+                requested_output_names=["predictions"],
+                inputs=[pb_utils.Tensor("input", array)],
+            )
+            m_response = m_request.exec()
+            if m_response.has_error():
+                raise pb_utils.TritonModelException(m_response.error().message())
+            outputs = self.get_outputs(".material", m_response, n_probe, top_k)
+            responses.append(pb_utils.InferenceResponse(output_tensors=outputs))
+        return responses
